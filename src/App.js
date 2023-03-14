b1b668fd3e772de7ec476ba7ab062e4722771e16
Ryan Howard <ryanghoward@protonmail.com>
2023-03-14 14:46:11 -0700

    save

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Coins from "./components/Coins";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -19,7 +21,12 @@ function App() {
       });
   });
 
-  return <>Hello</>;
+  return (
+    <>
+      <Navbar />
+      <Coins coins={coins} />
+    </>
+  );
 }
 
 export default App;
